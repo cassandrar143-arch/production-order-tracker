a124ab16851e6432788f0639b89977b573b59c66
Cassandra <cassandra@ip134-52.wlan.ntust.edu.tw>
2025-11-20 10:33:27 +0800

    Enable debug mode to trace internal error

--- app.py
+++ app.py
@@ -237,4 +237,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
-    
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
